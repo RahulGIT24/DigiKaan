34a5780dfc3b2072f3edaa7413cce3bd1e499f1c
rahul <rg4005450@gmail.com>
2024-01-14 23:05:40 +0530

    Setup the project and routes for frontend

---
+++ client/src/App.tsx
@@ -0,0 +1,93 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { lazy, Suspense } from "react";
+import Loader from "./components/Loader";
+import Header from "./components/Header";
+import OrderDetails from "./pages/orderDetails";
+
+const Home = lazy(() => import("./pages/home"));
+const Search = lazy(() => import("./pages/search"));
+const Cart = lazy(() => import("./pages/cart"));
+
+// Admin Routes Importing
+const Dashboard = lazy(() => import("./pages/admin/dashboard"));
+const Shipping = lazy(() => import("./pages/shipping"));
+const Products = lazy(() => import("./pages/admin/products"));
+const Customers = lazy(() => import("./pages/admin/customers"));
+const Transaction = lazy(() => import("./pages/admin/transaction"));
+const Barcharts = lazy(() => import("./pages/admin/charts/barcharts"));
+const Piecharts = lazy(() => import("./pages/admin/charts/piecharts"));
+const Linecharts = lazy(() => import("./pages/admin/charts/linecharts"));
+const Coupon = lazy(() => import("./pages/admin/apps/coupon"));
+const Login = lazy(() => import("./pages/login"));
+const Orders = lazy(() => import("./pages/orders"));
+const Stopwatch = lazy(() => import("./pages/admin/apps/stopwatch"));
+const Toss = lazy(() => import("./pages/admin/apps/toss"));
+const NewProduct = lazy(() => import("./pages/admin/management/newproduct"));
+const ProductManagement = lazy(
+  () => import("./pages/admin/management/productmanagement")
+);
+const TransactionManagement = lazy(
+  () => import("./pages/admin/management/transactionmanagement")
+);
+
+const App = () => {
+  return (
+    <Router>
+      {/* Header */}
+      <Header />
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/cart" element={<Cart />} />
+
+          {/* Not logged in route */}
+          <Route path="/login" element={<Login/>}/>
+
+          {/* Login user routes */}
+          <Route>
+            <Route path="/shipping" element={<Shipping />} />
+            <Route path="/orders" element={<Orders />} />
+            <Route path="/orderdetails/:id" element={<OrderDetails />} />
+          </Route>
+
+          {/* <Route
+          // element={
+          //   <ProtectedRoute
+          //     isAuthenticated={true}
+          //     adminRoute={true}
+          //     isAdmin={true}
+          //   />
+          // }
+          > */}
+          <Route path="/admin/dashboard" element={<Dashboard />} />
+          <Route path="/admin/product" element={<Products />} />
+          <Route path="/admin/customer" element={<Customers />} />
+          <Route path="/admin/transaction" element={<Transaction />} />
+          {/* Charts */}
+          <Route path="/admin/chart/bar" element={<Barcharts />} />
+          <Route path="/admin/chart/pie" element={<Piecharts />} />
+          <Route path="/admin/chart/line" element={<Linecharts />} />
+          {/* Apps */}
+          <Route path="/admin/app/coupon" element={<Coupon />} />
+          <Route path="/admin/app/stopwatch" element={<Stopwatch />} />
+          <Route path="/admin/app/toss" element={<Toss />} />
+
+          {/* Management */}
+          <Route path="/admin/product/new" element={<NewProduct />} />
+
+          <Route path="/admin/product/:id" element={<ProductManagement />} />
+
+          <Route
+            path="/admin/transaction/:id"
+            element={<TransactionManagement />}
+          />
+          {/* </Route> */}
+        </Routes>
+      </Suspense>
+      {/* Admin Routes */}
+    </Router>
+  );
+};
+
+export default App;
