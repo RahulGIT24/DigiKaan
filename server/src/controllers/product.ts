e41fd2725d6f7859782dc88fec231807773bf462
rahul <rg4005450@gmail.com>
2024-01-16 20:56:43 +0530

    Deleting photo from uploads

--- server/src/controllers/product.ts
+++ server/src/controllers/product.ts
@@ -2,16 +2,20 @@ import { Request } from "express";
 import ErrorHandler, { TryCatch } from "../utils/utility-class.js";
 import { NewProductRequestBody } from "../types/types.js";
 import { Product } from "../models/product.js";
+import { rm } from "fs";
 
 export const newProduct = TryCatch(
     async (req: Request<{}, {}, NewProductRequestBody>, res, next) => {
         const { name, category, stock, price } = req.body;
-        if(!name || !category || !stock || !price){
-            next(new ErrorHandler("Please fill all fields", 400));
-        }
         const photo = req.file;
         if (!photo) {
-            next(new ErrorHandler("Please upload the photo", 400));
+            return next(new ErrorHandler("Please upload the photo", 400));
+        }
+        if (!name || !category || !stock || !price) {
+            rm(photo?.path, () => {
+                console.log("Deleted");
+            })
+            return next(new ErrorHandler("Please fill all fields", 400));
         }
         await Product.create({
             name, category: category?.toLowerCase(), stock, price, photo: photo?.path
