84ee3097ef56e720bb00e6ee0e2098e4e03d02bb
rahul <rg4005450@gmail.com>
2024-01-21 23:11:34 +0530

    React Loader

--- client/src/components/Loader.tsx
+++ client/src/components/Loader.tsx
@@ -1,9 +1,19 @@
+import { Hourglass } from "react-loader-spinner";
+
 const Loader = () => {
   return (
-    <div>
-      Loading
+    <div className="loader">
+      <Hourglass
+        visible={true}
+        height="80"
+        width="80"
+        ariaLabel="hourglass-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        colors={["black", "white"]}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Loader
+export default Loader;
