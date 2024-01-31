9b595bc5c4395fab8f283014a3a4fb2ef458af8f
rahul <rg4005450@gmail.com>
2024-01-31 09:00:55 +0530

    Minor change in product card

--- client/src/components/ProductCard.tsx
+++ client/src/components/ProductCard.tsx
@@ -1,5 +1,8 @@
-import { FaPlus } from "react-icons/fa";
+import { FaPlus, FaSignInAlt } from "react-icons/fa";
 import { CartItem } from "../types/common";
+import { UserReducerInitialState } from "../types/reducer";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 type ProductProps = {
   productId: string;
@@ -10,6 +13,7 @@ type ProductProps = {
   handler: (cartItem: CartItem) => string | undefined;
 };
 
+
 const ProductCard = ({
   photo,
   price,
@@ -18,12 +22,18 @@ const ProductCard = ({
   productId,
   stock,
 }: ProductProps) => {
+  const { user } = useSelector(
+    (state: { userReducer: UserReducerInitialState }) => state.userReducer
+  );
+  const navigate = useNavigate();
   return (
     <div className="product-card">
       <img src={`${photo}`} alt="" />
       <p>{name}</p>
       <span>₹{price}</span>
       <div>
+        {
+          user?._id ? 
         <button
           onClick={() =>
             handler({
@@ -37,7 +47,14 @@ const ProductCard = ({
           }
         >
           <FaPlus />
+        </button>:<>
+        <button
+          onClick={() =>{navigate("/login")}}
+        >
+          <FaSignInAlt />
         </button>
+        </>
+        }
       </div>
     </div>
   );
